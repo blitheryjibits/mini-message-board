cff0bb9c33a49075527c2b646021294a7a1d76dc
robert thornton <robert.thornton92@gmail.com>
2025-07-04 10:28:03 +0300

    Refactor: changed inline functions to imported controller functions.

--- routers/indexRouter.js
+++ routers/indexRouter.js
@@ -1,39 +1,28 @@
 
 const { Router } = require('express');
 const indexRouter = Router();
+const controller = require('../controllers/controller');
 
 // sample messages
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+// const messages = [
+//   {
+//     text: "Hi there!",
+//     user: "Amando",
+//     added: new Date()
+//   },
+//   {
+//     text: "Hello World!",
+//     user: "Charles",
+//     added: new Date()
+//   }
+// ];
 
-indexRouter.get('/', (req, res) => {
-  res.render('indexTemplate', { messages });
-});
+indexRouter.get('/', controller.getMessages);
 
 indexRouter.get('/new', (req, res) => {
   res.render('form');
 });
 
-indexRouter.post('/new', (req, res) => {
-  const { userInput, messageInput } = req.body;
-
-  messages.push({
-    user: userInput,
-    text: messageInput,
-    added: new Date()
-  });
-  
-  res.redirect('/');
-});
+indexRouter.post('/new', controller.addMessage);
 
 module.exports = indexRouter;
